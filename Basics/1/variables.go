03f08c8e990ee43eebe4a803753c68f328d69953
Anurag <annurag94@gmail.com>
2020-11-29 16:06:02 +0530

    Variables

--- Basics/1/variables.go
+++ Basics/1/variables.go
@@ -2,34 +2,88 @@ package main
 
 import (
 	"fmt"
-	"reflect"
+	"strconv"
 )
 
-// VARIABLES
+var v int = 32
+
+var (
+	name      string = "My Name"
+	jobTitle         = "Engineer"
+	otherInfo        = "Extra Info"
+)
+
+var a int = 42
 
 func main() {
-	// variable decleration
-	var name string = "MICROSOFT"
-	var price float64
-	var quant int
+	fmt.Println("Variables")
 
-	// variable assignment
-	price, quant = 1000.23, 5
+	// 1. Declaring variable
+	var i int
+	fmt.Println(i)
 
-	// shorthand decleration
-	profit := 100
+	// 2. Assigning a value
+	i = 42
 
-	var loss bool
+	// 3. Value of a variable can be changed
+	i = 21
 
-	totalCost := price * float64(quant)
+	// 4. Combine declration and assignment
+	var j int = 42
+	fmt.Println(j)
 
-	fmt.Println(reflect.TypeOf(quant))
-	fmt.Println(reflect.TypeOf(float64(quant)))
+	// 5. Shorthand assignment
+	k := 42
+	fmt.Println(k)
 
-	fmt.Println(name)
-	fmt.Println(price)
-	fmt.Println(quant)
-	fmt.Println(profit)
-	fmt.Println(loss)
-	fmt.Println(totalCost)
+	// 6. Assigning wrong type (Go is strongly and statically typed)
+	var s string
+	s = "characters"
+	// s = 42
+	fmt.Println(s)
+
+	// 7. Declare at package level (cant use shorthand assignment)
+
+	// 8. Variable block to clean clutter
+	var name string = "My Name"
+	var jobTitle string = "Engineer"
+	var otherInfo string = "Extra Info"
+	fmt.Println(name, jobTitle, otherInfo)
+
+	// 9. Redeclaring varibales in the same scope (Shadowing)
+	fmt.Println(a)
+	var a int = 420
+	fmt.Println(a)
+	// var a int = 42
+
+	// 10. Variables always have to be used
+
+	// 11. Levels of scoping
+	// local, package, global
+	// > Lowercase variables scoped to package
+	// > Uppercase variables exposed outside package
+
+	// 11. Naming conventions - 1
+	// concise name for loops etc
+	// descriptive names for variables used for longer time
+	// verbose names for package level variables
+	// acronyms should be all upper case Eg. EmpID (for readability)
+
+	// 12. Type conversion has to be explicit
+	var b int = 42
+	fmt.Printf("Value = %v, Type = %T\n", b, b)
+
+	var c float64 = float64(b)
+	fmt.Printf("Value = %v, Type = %T\n", b, b)
+
+	var d int = int(c)
+	fmt.Printf("Value = %v, Type = %T\n", d, d)
+
+	// 13. Convert int to string using strconv
+	var e string
+	e = string(a)
+	fmt.Printf("Value = %v, Type = %T\n", e, e)
+	e = strconv.Itoa(a)
+	fmt.Printf("Value = %v, Type = %T\n", e, e)
+
 }
