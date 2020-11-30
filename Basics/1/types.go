ea0c97d445eb7a19e6badcf25ee556a4dfd6cc04
Anurag <annurag94@gmail.com>
2020-11-30 17:59:10 +0530

    Types

--- Basics/1/types.go
+++ Basics/1/types.go
@@ -16,7 +16,7 @@ func main() {
 	// int - unspecified size, based on platform implementation
 	fmt.Printf("Value = %v, Type = %T\n", 42, 42)
 
-	// 3. Integers of specific sizes
+	// Integers of specific sizes
 	// int8, int16, int32, int64
 	// int8		8 bits		-128 to 127
 	// int16	16 bits		-32768 to 32767
@@ -24,14 +24,14 @@ func main() {
 	// int64	64 bits		-2^63 to (2^63)-1
 	// Use math Big Package for larger numbers: https://golang.org/pkg/math/big/
 
-	// 4. Unsigned Integers
+	// Unsigned Integers
 	// uint8	8 bits		0 to 255
 	// uint16	16 bits		0 to 65535
 	// uint32	32 bits		0 to 4294967295
 
 	// 5. Can't add values of different types
 
-	// 6. To store decimal numbers:
+	// To store decimal numbers:
 	// Floating point numbers IEEE754 standard float32, float64
 	// float32	32 bits	 +-1.18E-38 to +-3.4E36
 	// float64	64 bits	 +-2.23E-308 to +-1.8E308
@@ -49,22 +49,5 @@ func main() {
 
 	var m complex128 = complex(3, 4)
 	fmt.Printf("Value = %v, Type = %T\n", m, m)
-
-	// 8. Strings
-	var s string = "this is a string"
-	// treated like an array (collection or array)
-	fmt.Printf("Value = %v, Type = %T\n", s, s)
-	fmt.Printf("Value = %v, Type = %T\n", s[0], s[0])
-	// s[2] = 'o'
-	// concat using +
-
-	// convert them into a collection/slice of bytes
-	b := []byte(s)
-	fmt.Printf("Value = %v, Type = %T\n", b, b)
-
-	// 9. Rune (utf-32 characters)
-	// Type alias for int32
-	var r rune = 'a'
-	fmt.Printf("Value = %v, Type = %T\n", r, r)
 
 }
