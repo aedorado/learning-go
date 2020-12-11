13ec534e799567851cac6dc2569a03a28a59cab3
Anurag <annurag94@gmail.com>
2020-12-11 19:21:31 +0530

    Methods

--- Basics/1/methods.go
+++ Basics/1/methods.go
@@ -4,34 +4,34 @@ import (
 	"fmt"
 )
 
-type greeter struct {
-	greeting string
-	name     string
+type Person struct {
+	name string
+	age  int
 }
 
 // function that is executing in a known context (kc -> any type)
-// recieves a copy of the context ie. the greeter struct
-func (g greeter) greet() {
-	fmt.Println(g.greeting, g.name)
+// recieves a copy of the context ie. the Person struct
+func (g Person) details() {
+	fmt.Println(g.name, g.age)
 }
 
 func main() {
 	fmt.Println("Methods")
-	g := greeter{
-		greeting: "Hello",
-		name:     "Go",
+	g := Person{
+		age:  25,
+		name: "James",
 	}
 	g.greet()
 }
 
 // value reciever
-func (g greeter) greetVal() {
+func (g Person) greetVal() {
 	g.name = "does not affect original"
-	fmt.Println(g.greeting, g.name)
+	fmt.Println(g.age, g.name)
 }
 
 // pointer
-func (g *greeter) greetPtr() {
-	g.name = "does not affect original"
-	fmt.Println(g.greeting, g.name)
+func (g *Person) greetPtr() {
+	g.name = "does affect original"
+	fmt.Println(g.age, g.name)
 }
