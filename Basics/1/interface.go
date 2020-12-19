dfa656afa8a5cde2d597c3290e77b0c55cf02efe
Anurag <annurag94@gmail.com>
2020-12-19 19:29:25 +0530

    Interfaces

--- Basics/1/interface.go
+++ Basics/1/interface.go
@@ -12,42 +12,51 @@ type Circle struct {
 func (c Circle) area() float64 {
 	return math.Pi * c.radius * c.radius
 }
+func (c Circle) perim() float64 {
+	return 2 * math.Pi * c.radius
+}
 
-type Rectangle struct {
+type Rect struct {
 	length, width float64
 }
 
-func (r Rectangle) area() float64 {
+func (r Rect) area() float64 {
 	return r.length * r.width
+}
+func (r Rect) perim() float64 {
+	return 2*r.length + 2*r.width
+}
+
+func measure(s Shape) {
+	fmt.Println(s)
+	fmt.Println("Area: ", s.area())
+	fmt.Println("Perimeter: ", s.perim())
 }
 
 type Shape interface {
 	area() float64
+	perim() float64
 }
 
 func main() {
 	// 1. Example, implicit implementation
 	fmt.Println("Hello, playground")
 	c1 := Circle{radius: 1}
-	c2 := Circle{radius: 2}
-	r1 := Rectangle{length: 5, width: 2}
-	r2 := Rectangle{length: 2, width: 3}
-	circles := []Circle{c1, c2}
-	rectangles := []Rectangle{r1, r2}
-	allShapes := []Shape{c1, c2, r1, r2}
-	fmt.Println(totalArea(circles, rectangles))
-	fmt.Println(totalAreaI(allShapes))
+	r1 := Rect{length: 5, width: 2}
+
+	allShapes := []Shape{c1, r1}
+	fmt.Printf("%+v\n", allShapes)
 
 	// 2. Type Conversion
 	var c Shape = Circle{radius: 1}
 	c3 := c.(Circle)
 	fmt.Printf("Value = %+v, Type = %T\n", c3, c3)
 
-	// r3 := c.(Rectangle)
+	// r3 := c.(Rect)
 	// fmt.Printf("Value = %+v, Type = %T\n", r3, r3)
 
 	// 3. Avoid panicing
-	r3, ok := c.(Rectangle)
+	r3, ok := c.(Rect)
 	if ok {
 		fmt.Printf("Value = %+v, Type = %T\n", r3, r3)
 	} else {
@@ -66,23 +75,4 @@ func main() {
 	} else {
 		fmt.Printf("Cannot convert\n")
 	}
-}
-
-func totalArea(circles []Circle, rectangles []Rectangle) float64 {
-	var total float64
-	for _, c := range circles {
-		total += c.area()
-	}
-	for _, r := range rectangles {
-		total += r.area()
-	}
-	return total
-}
-
-func totalAreaI(shapes []Shape) float64 {
-	var area float64
-	for _, s := range shapes {
-		area += s.area()
-	}
-	return area
 }
